6ac305f353e51fff3d007d6b7d42e3174d2bbffc
Paraita Wohler <paraita.wohler@activeeon.com>
2017-09-19 08:43:52 +0200

    Replace the cpu load by the total num of requests in progress

--- linecounter.py
+++ linecounter.py
@@ -3,8 +3,8 @@
 from flask import Flask
 from flask_restplus import Resource, Api, reqparse
 from werkzeug.datastructures import FileStorage
+from time import sleep
 import socket
-import psutil
 
 app = Flask(__name__)
 api = Api(app, version='1.0', title='LineCounter', description='This REST service exposes an endpoint that counts the lines of a given text file')
@@ -12,16 +12,22 @@ api = Api(app, version='1.0', title='LineCounter', description='This REST servic
 upload_parser = api.parser()
 upload_parser.add_argument('text', location='files', type=FileStorage)
 
+requests_list = []
+
 @api.route('/lines')
 @api.expect(upload_parser)
 class LineCounter(Resource):
     def post(self):
+        requests_list.append(1)
         args = upload_parser.parse_args()
         text = args['text']
+        sleep(5)
         if text:
             result = len(text.read().decode('utf-8').split('\n'))
+            requests_list.pop()
             return {'count': result}
         else:
+            requests_list.pop()
             return {'count': 0}
 
 @api.route('/info')
@@ -30,8 +36,8 @@ class ServiceName(Resource):
         return {
             'service_name': 'WordCounter',
             'hostname': socket.gethostname(),
-            'cpu_load': psutil.cpu_percent()
+            'nb_requests': len(requests_list)
         }
     
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(threaded=True, host='0.0.0.0')
--- wordcounter.py
+++ wordcounter.py
@@ -3,8 +3,8 @@
 from flask import Flask
 from flask_restplus import Resource, Api, reqparse
 from werkzeug.datastructures import FileStorage
+from time import sleep
 import socket
-import psutil
 
 app = Flask(__name__)
 api = Api(app, version='1.0', title='WordCounter', description='This REST service exposes an endpoint that counts the words of a given text file')
@@ -12,16 +12,22 @@ api = Api(app, version='1.0', title='WordCounter', description='This REST servic
 upload_parser = api.parser()
 upload_parser.add_argument('text', location='files', type=FileStorage)
 
+requests_list = []
+
 @api.route('/words')
 @api.expect(upload_parser)
 class WordCounter(Resource):
     def post(self):
+        requests_list.append(1)
         args = upload_parser.parse_args()
         text = args['text']
+        sleep(5)
         if text:
             result = len(text.read().decode('utf-8').split())
+            requests_list.pop()
             return {'count': result}
         else:
+            requests_list.pop()
             return {'count': 0}
 
 @api.route('/info')
@@ -30,8 +36,8 @@ class ServiceName(Resource):
         return {
             'service_name': 'WordCounter',
             'hostname': socket.gethostname(),
-            'cpu_load': psutil.cpu_percent()
+            'nb_requests': len(requests_list)
         }
-    
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(threaded=True, host='0.0.0.0')
